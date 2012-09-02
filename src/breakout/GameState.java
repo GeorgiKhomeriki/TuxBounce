500e8d2753f96e4fe10ea8050c14dcf44ef7acfb
Georgi Khomeriki <g.khomeriki@gmail.com>
2012-09-02 23:41:23 +0200

    removed Level class

--- src/breakout/GameState.java
+++ src/breakout/GameState.java
@@ -20,16 +20,27 @@ import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
+import breakout.Block.BlockState;
+
+import particles.Particles;
+import particles.SimpleExplosion;
+import util.LevelLoader;
+
 import engine.IGameState;
+import engine.Texts;
 
 public class GameState implements IGameState {
 
 	public static final String name = "STATE_GAME";
 
 	private Texture bgTexture;
-	private Level level;
 	private Paddle paddle;
 	private List<Ball> balls;
+	private List<Block> blocks;
+	private List<Particles> particles;
+	private List<Coin> coins;
+	private Texture[] coinTextures;
+	private Texts texts;
 
 	@Override
 	public String getName() {
@@ -46,16 +57,29 @@ public class GameState implements IGameState {
 
 		paddle = new Paddle(100, 10, Display.getWidth() / 6,
 				Display.getHeight() / 20);
-		level = new Level("resources/levels/level1.txt", Hud.height);
 		balls = new ArrayList<Ball>();
 		balls.add(new Ball(Display.getWidth() / 2, Display.getHeight() / 2,
 				Display.getWidth() / 30, Display.getWidth() / 8, Display
 						.getHeight() / 6));
+
+		blocks = LevelLoader.load("resources/levels/level1.txt", Hud.height);
+		particles = new ArrayList<Particles>();
+		coins = new ArrayList<Coin>();
+		texts = new Texts();
 	}
 
 	private void loadTextures() throws IOException {
 		bgTexture = TextureLoader.getTexture("PNG", ResourceLoader
 				.getResourceAsStream("resources/images/coldmountain.png"));
+		coinTextures = new Texture[4];
+		coinTextures[0] = TextureLoader.getTexture("PNG", ResourceLoader
+				.getResourceAsStream("resources/images/coinBlue.png"));
+		coinTextures[1] = TextureLoader.getTexture("PNG", ResourceLoader
+				.getResourceAsStream("resources/images/coinRed.png"));
+		coinTextures[2] = TextureLoader.getTexture("PNG", ResourceLoader
+				.getResourceAsStream("resources/images/coinGreen.png"));
+		coinTextures[3] = TextureLoader.getTexture("PNG", ResourceLoader
+				.getResourceAsStream("resources/images/coinGrey.png"));
 	}
 
 	@Override
@@ -81,7 +105,19 @@ public class GameState implements IGameState {
 			ball.render();
 		}
 
-		level.render();
+		for (Block block : blocks) {
+			block.render();
+		}
+
+		for (Particles p : particles) {
+			p.render();
+		}
+
+		for (Coin c : coins) {
+			c.render();
+		}
+
+		texts.render();
 
 		Hud.get().render();
 	}
@@ -113,9 +149,92 @@ public class GameState implements IGameState {
 			ball.update(delta, paddle);
 		}
 
-		level.update(delta, paddle, balls);
+		// update blocks
+		for (int i = 0; i < blocks.size(); i++) {
+			Block block = blocks.get(i);
+			// TODO: this iterartion sucks!
+			for (Ball ball : balls) {
+				if (block.getState().equals(BlockState.ALIVE)
+						&& block.isHit(ball)) {
+					ball.bounce(block);
+					block.onHit();
+					spawnParticles(block);
+					spawnCoin(block);
+				} else if (block.getState().equals(BlockState.DEAD)) {
+					blocks.remove(block);
+					i--;
+					break;
+				} else {
+					block.update(delta);
+				}
+			}
+		}
+
+		// update particles
+		for (int i = 0; i < particles.size(); i++) {
+			Particles p = particles.get(i);
+			if (p.isAlive()) {
+				p.update(delta, paddle, texts);
+			} else {
+				particles.remove(i);
+				i--;
+			}
+		}
+
+		// update coins
+		for (int i = 0; i < coins.size(); i++) {
+			Coin c = coins.get(i);
+			if (c.isAlive()) {
+				if (c.isHit(paddle)) {
+					Hud.get().addPoints(10);
+					texts.add("+10", c.getX(), c.getY(), 30, true);
+					balls.add(new Ball(Display.getWidth() / 2, Display
+							.getHeight() / 2, Display.getWidth() / 30, Display
+							.getWidth() / 8, Display.getHeight() / 6));
+					coins.remove(i);
+					i--;
+				} else {
+					c.update(delta);
+				}
+			} else {
+				coins.remove(i);
+				i--;
+			}
+		}
+
+		// update texts
+		texts.update(delta);
 
 		Hud.get().update(delta);
+	}
+
+	private void spawnParticles(Block block) {
+		particles.add(new SimpleExplosion(5, block.getTexture(), block.getX()
+				+ 0.5f * Block.width, block.getY() - 0.5f * Block.height, 0.4f,
+				0.4f, 0.4f, 1.0f, 1.0f, 1.0f));
+	}
+
+	private void spawnCoin(Block block) {
+		switch (block.getType()) {
+		case BLUE_FACE:
+			coins.add(new Coin(coinTextures[0], block.getX(), block.getY(),
+					Block.width, Block.height, 0.0f, -40.0f));
+			break;
+		case RED_FACE:
+			coins.add(new Coin(coinTextures[1], block.getX(), block.getY(),
+					Block.width, Block.height, 0.0f, -40.0f));
+			break;
+		case GREEN_FACE:
+			coins.add(new Coin(coinTextures[2], block.getX(), block.getY(),
+					Block.width, Block.height, 0.0f, -40.0f));
+			break;
+		case GREY_FACE:
+			coins.add(new Coin(coinTextures[3], block.getX(), block.getY(),
+					Block.width, Block.height, 0.0f, -40.0f));
+			break;
+		default:
+			break;
+		}
 	}
 
 }
