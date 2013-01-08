bc2960f436fc7c8d74ba8e64dd44d471bcc25943
Georgi Khomeriki <g.khomeriki@gmail.com>
2013-01-08 20:02:53 +0100

    updated version to 0.6

--- build-and-run.sh
+++ build-and-run.sh
@@ -3,7 +3,7 @@ echo "Starting maven build"
 mvn clean install
 cd target
 echo "Extracting release archive"
-unzip TuxBounce-0.01-release.zip
-cd TuxBounce-0.01
+unzip TuxBounce-0.6-release.zip
+cd TuxBounce-0.6
 echo "Run application"
 ./run.sh
